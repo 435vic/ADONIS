973f877072ff6760b1e57a9f68bccdb1baa74930
Victor Javier <victor_javier2004@hotmail.com>
2023-04-17 23:16:35 -0600

    asfasf

--- src/python/camera.py
+++ src/python/camera.py
@@ -1,4 +1,4 @@
-# from picamera2 import Picamera2
+from picamera2 import Picamera2
 import cv2
 
 class Camera:
@@ -10,15 +10,15 @@ class Camera:
 
 class PiCamera(Camera):
     def setup(self):
-        # self.camera = Picamera2()
-        # self.camera.configure(
-        #     self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)})
-        # )
-        # self.camera.start()
+        self.camera = Picamera2()
+        self.camera.configure(
+            self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)})
+        )
+        self.camera.start()
         pass
 
     def get_frame(self):
-        # return (True, self.camera.capture_array())
+        return (True, self.camera.capture_array())
         return False, None
     
 class Webcam(Camera):
